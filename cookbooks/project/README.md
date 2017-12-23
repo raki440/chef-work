ecc7b57dd73dd75e7b21877b6774fec99adefef3
root <root@node2.(none)>
2017-12-23 13:28:07 +0000

    this is committed changes

---
+++ cookbooks/project/README.md
@@ -0,0 +1,4 @@
+# project
+
+TODO: Enter the cookbook description here.
+
--- cookbooks/project/metadata.rb
+++ cookbooks/project/metadata.rb
@@ -1,9 +1,9 @@
-name 'test'
+name 'project'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
-description 'Installs/Configures test'
-long_description 'Installs/Configures test'
+description 'Installs/Configures project'
+long_description 'Installs/Configures project'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
@@ -11,12 +11,10 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/test/issues'
+# issues_url 'https://github.com/<insert_org_here>/project/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/test'
-depends 'apt', '~> 5.0'
-
+# source_url 'https://github.com/<insert_org_here>/project'
