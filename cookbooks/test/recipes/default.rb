efad82a8166364f4bed3adbf041373c505a74239
root <root@node2.(none)>
2017-12-23 07:17:25 +0000

    test cookbook updated with file resource updated

--- cookbooks/test/recipes/default.rb
+++ cookbooks/test/recipes/default.rb
@@ -4,6 +4,6 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 file '/var/index.php' do
-  content '<html>This is a placeholder for the home page.</html>'
+  content '<html>This is my home page.</html>'
   mode '0755'
 end
